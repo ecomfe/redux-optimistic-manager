8a3bb220ca37826160d033b45079270b7304af11
otakustay <otakustay@gmail.com>
2018-04-18 00:28:58 +0800

    Fix an issue where babel doesn't transform object rest spread

--- example/cli/index.js
+++ example/cli/index.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console, import/no-extraneous-dependencies */
+/* eslint-disable no-console */
 
 import {createStore, applyMiddleware} from 'redux';
 import chalk from 'chalk';
